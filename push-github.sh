57ad28224228a40220d9f21f74cf1c7b7cd9cc63
fferrarigabriel <fferrarigabriel@gmail.com>
2023-07-30 18:00:12 -0300

    novo commit

---
+++ push-github.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## token: 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
+git init 
+git add . 
+git commit -m "novo commit"
+git branch -M main 
+git remote add origin https://github.com/fferrarigabriel/fferrarigabriel.github.io.git
+git push -u origin main 
